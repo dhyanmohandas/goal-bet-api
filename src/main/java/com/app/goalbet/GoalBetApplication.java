6b233dbde6e77a85ff763569cb58c036fee64d8b
Dhyan Mohandas <dhyanmohandas@gmail.com>
2021-05-28 21:49:05 +0530

    Initial Commit

---
+++ src/main/java/com/app/goalbet/GoalBetApplication.java
@@ -0,0 +1,13 @@
+package com.app.goalbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalBetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/goalbet/GoalBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.goalbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
