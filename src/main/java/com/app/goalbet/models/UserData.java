b93914dd4b8bbcbd449f576f7a8f79748d13964f
Dhyan Mohandas <dhyanmohandas@gmail.com>
2021-06-06 21:31:06 +0530

    Resttemplate update

--- src/main/java/com/app/goalbet/models/UserData.java
+++ src/main/java/com/app/goalbet/models/UserData.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * UserData
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-06-06T16:02:24.027922400+05:30[Asia/Calcutta]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-06-06T21:22:31.492762200+05:30[Asia/Calcutta]")
 public class UserData   {
   @JsonProperty("id")
   private Integer id = null;
--- src/main/java/com/app/goalbet/utility/RestApiUtility.java
+++ src/main/java/com/app/goalbet/utility/RestApiUtility.java
@@ -2,33 +2,49 @@ package com.app.goalbet.utility;
 
 import java.util.Arrays;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import javax.net.ssl.SSLContext;
 
 import com.app.goalbet.models.PredictionDetails;
 
 @Component
 public class RestApiUtility {
 	public ResponseEntity<String> getRestTemplateResponse(String url, HttpMethod httpMethod, String apiKey) {
-		RestTemplate restTemplate = new RestTemplate();
+		RestTemplate restTemplate = getRestTemplate();
 		HttpHeaders headers = getHttpHeaders(apiKey);
 		HttpEntity<String> entity = new HttpEntity<String>(headers);
-		ResponseEntity<String> response = restTemplate.exchange(url, httpMethod, entity, String.class);		
+		ResponseEntity<String> response = restTemplate.exchange(url, httpMethod, entity, String.class);
 		return response;
 	}
 
 	public ResponseEntity<String> postRestTemplateResponse(String url, HttpMethod httpMethod, String apiKey,
 			PredictionDetails predictionDetails) {
-		RestTemplate restTemplate = new RestTemplate();
+		RestTemplate restTemplate = getRestTemplate();
 		HttpHeaders headers = getHttpHeaders(apiKey);
 		HttpEntity<PredictionDetails> entity = new HttpEntity<PredictionDetails>(predictionDetails, headers);
 		ResponseEntity<String> response = restTemplate.exchange(url, httpMethod, entity, String.class);
 		return response;
+	}
+	
+	private RestTemplate getRestTemplate() {
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLHostnameVerifier(new NoopHostnameVerifier())
+				.build();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setHttpClient(httpClient);
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
+		return restTemplate;
 	}
 
 	private HttpHeaders getHttpHeaders(String apiKey) {
@@ -37,7 +53,8 @@ public class RestApiUtility {
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		headers.set("Accept", "application/json");
 		headers.set("x-api-key", apiKey);
-		headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+		headers.add("user-agent",
+				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
 		return headers;
 	}
 }
