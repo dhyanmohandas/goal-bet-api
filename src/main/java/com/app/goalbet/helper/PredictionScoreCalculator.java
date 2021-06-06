3cb76805480832d742f1fb9bd82075f06497cf19
Vishak Prasad C <pcvishak@gmail.com>
2021-06-06 22:56:22 +0530

    Added pseudocode for loss calculation.
    Signed-off-by: Vishak Prasad C <pcvishak@gmail.com>

--- src/main/java/com/app/goalbet/helper/PredictionScoreCalculator.java
+++ src/main/java/com/app/goalbet/helper/PredictionScoreCalculator.java
@@ -3,10 +3,25 @@ package com.app.goalbet.helper;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PredictionScoreCalculator {
+public class PredictionLossCalculator {
+
+	correct_prediction(int originalGoal1, int originalGoal2, int predictedGoal1, int predictedGoal2)
+	{
+		if ((originalGoal1 - originalGoal2) * (predictedGoal1 - predictedGoal2) > 0) or ((originalGoal1 - originalGoal2) == 0 and (predictedGoal1 - predictedGoal2) == 0)
+			return 1;
+	}
 	
-	public int calculate(int teamGoals1, int teamGoals2) {
-		return 0;
+	public int calculate(int originalGoal1, int originalGoal2, int predictedGoal1, int predictedGoal2) {
+
+		int penalty_for_incorrectness = 5;
+		int loss = 0;
+		loss = Math.abs(originalGoal1 - predictedGoal1) + Math.abs(originalGoal2 - predictedGoal2) + Math.abs((originalGoal1 - originalGoal2) - (predictedGoal1 - predictedGoal2))
+
+		if !(correct_prediction(int originalGoal1, int originalGoal2, int predictedGoal1, int predictedGoal2))
+		{
+			loss = loss + penalty_for_incorrectness;
+		}
+		return loss;
 	}
 
 }
