3114e6399dd6624c129e3f7db8113033823cbfbc
Dhyan Mohandas <dhyanmohandas@gmail.com>
2021-06-12 10:36:49 +0530

    Cors update

--- src/main/java/com/app/goalbet/models/PredictionDetails.java
+++ src/main/java/com/app/goalbet/models/PredictionDetails.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * PredictionDetails
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-06-12T00:07:32.114442800+05:30[Asia/Calcutta]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-06-12T10:34:51.036803300+05:30[Asia/Calcutta]")
 public class PredictionDetails   {
   @JsonProperty("matchId")
   private Integer matchId = null;
