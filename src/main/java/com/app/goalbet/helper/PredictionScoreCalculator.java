b28c2a9e4b3b5fbf64cf3630ed3b4fadf9e32d60
Vishak Prasad C <pcvishak@gmail.com>
2021-06-12 14:52:59 +0530

    [WIP] Added score calculation
    Signed-off-by: Vishak Prasad C <pcvishak@gmail.com>

--- src/main/java/com/app/goalbet/helper/PredictionScoreCalculator.java
+++ src/main/java/com/app/goalbet/helper/PredictionScoreCalculator.java
@@ -18,7 +18,7 @@ public class PredictionScoreCalculator {
 		return false;
 	}
 	
-	public int calculate(int originalGoal1, int originalGoal2, int predictedGoal1, int predictedGoal2) {
+	public int calculateLoss(int originalGoal1, int originalGoal2, int predictedGoal1, int predictedGoal2) {
 		int loss = 0;
 		loss = Math.abs(originalGoal1 - predictedGoal1) + Math.abs(originalGoal2 - predictedGoal2) + Math.abs((originalGoal1 - originalGoal2) - (predictedGoal1 - predictedGoal2));
 
@@ -28,5 +28,46 @@ public class PredictionScoreCalculator {
 		}
 		return loss;
 	}
+
+
+	public static double calculateSD(float numArray[])
+    {
+        double sum = 0.0, standardDeviation = 0.0;
+        int length = numArray.length;
+
+        for(double num : numArray) {
+            sum += num;
+        }
+
+        double mean = sum/length;
+
+        for(double num: numArray) {
+            standardDeviation += Math.pow(num - mean, 2);
+        }
+
+        return Math.sqrt(standardDeviation/length);
+    }
+
+	public int calculateMedian(float losses[]) {
+		Arrays.sort(losses);
+		double median;
+		if (losses.length % 2 == 0)
+		    median = ((double)losses[losses.length/2] + (double)losses[losses.length/2 - 1])/2;
+		else
+		    median = (double) losses[losses.length/2];
+
+		return median;
+	}
+
+	public int[] calculateScore(float losses[]){
+		int median = calculateMedian(losses);
+		int standardDeviation = calculateSD(losses);
+		for (i=0; i<losses.length; i++) {
+		  losses[i] = (median - losses[i]) / standardDeviation;
+		}
+		return losses;
+	}
+
+
 
 }
